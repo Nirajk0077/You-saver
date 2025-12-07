65141d71b4dc410bae1db48668a5b65d4e46cca5
surya808493 <surajkumar75419696@gmail.com>
2025-12-08 00:53:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
