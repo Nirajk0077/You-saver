4bff310a9e271edc6331c449b95481874f228090
surya808493 <surajkumar75419696@gmail.com>
2025-12-08 04:57:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "29285954"))
-    API_HASH = os.environ.get("API_HASH", "ee47f0d64723ee82fe3968ab09efbe3c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8481420396:AAHmjq0q6bR5BBYrIyTpk1pybiW2LE0amX8")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     OWNER_ID = int(os.environ.get("OWNER_ID", "8070498307"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003451702258"))
     UPI_ID = os.environ.get("UPI_ID", "deendayalsurajk@axl")
