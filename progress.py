3db2336c1af1efdd8abb742495aad6158d8ca304
surya808493 <surajkumar75419696@gmail.com>
2025-12-09 01:39:13 +0530

    Add files via upload

---
+++ progress.py
@@ -0,0 +1,73 @@
+import math
+import time
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import StopTransmission
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+def time_formatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s") if seconds else "")
+    return tmp[:-2] if tmp else "0s"
+
+async def progress_for_pyrogram(current, total, ud_type, message, start, check_cancel=None, force=False):
+    if check_cancel and check_cancel():
+        raise StopTransmission
+
+    now = time.time()
+    diff = now - start
+    if force or round(diff % 5.00) == 0 or current == total:
+        if total == 0:
+             return
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+
+        # Format time
+        estimated_total_time = time_formatter(time_to_completion)
+
+        progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
+            ''.join(["●" for i in range(math.floor(percentage / 5))]),
+            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2))
+
+        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            estimated_total_time if estimated_total_time != '' else "0s"
+        )
+
+        buttons = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("🔄 Refresh", callback_data="refresh_status"),
+                InlineKeyboardButton("❌ Cancel", callback_data=f"cancel_dl_{message.chat.id}_{message.id}")
+            ]
+        ])
+
+        try:
+            await message.edit(
+                text="{}\n{}".format(
+                    ud_type,
+                    tmp
+                ),
+                reply_markup=buttons
+            )
+        except:
+            pass
