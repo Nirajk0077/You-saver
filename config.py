56a0d32df8527afcd1ec0d5af04c2f16ec3978cd
Nirajk0077 <indok666@gmail.com>
2025-12-09 13:17:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
-    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "0"))
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://onemongodb_db_user:fIeTRLqivxrrtoiV@cluster0.rpyeonn.mongodb.net/?appName=Cluster0")
+    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1003435284491"))
     # ADMIN_IDS: comma separated list of user IDs
-    ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",") if x.isdigit()]
-    CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "")
+    ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "8070498307").split(",") if x.isdigit()]
+    CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/Deendayal_dhakadd")
